9051720c7484eddb526aa3005836042fa644cba3
Ryan Gammon <rgammon@microsoft.com>
2020-01-16 16:29:57 -0800

    Add KeyVault

--- RelayBotSample/Program.cs
+++ RelayBotSample/Program.cs
@@ -3,6 +3,10 @@
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Microsoft.PowerVirtualAgents.Samples.RelayBotSample
 {
@@ -10,11 +14,31 @@ namespace Microsoft.PowerVirtualAgents.Samples.RelayBotSample
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment;
+
+                    var builtConfig = config.Build();
+
+                    if (env.IsProduction())
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider(builtConfig["AzureServicesAuthConnectionString"]);
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        config.AddAzureKeyVault(
+                            $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                            keyVaultClient,
+                            new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
--- RelayBotSample/Startup.cs
+++ RelayBotSample/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.PowerVirtualAgents.Samples.RelayBotSample.Bots;
